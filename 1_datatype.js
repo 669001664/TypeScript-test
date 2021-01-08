e18a0f8d212adbb7deaf801902631c401623ffaf
moonlight <669001664@qq.com>
2021-01-08 11:20:48 +0800

    ts模块解决变量声明

---
+++ 1_datatype.js
@@ -0,0 +1,5 @@
+let a = 5;
+let b = 'abc';
+let c = true;
+console.log(a, b, c);
+export {};
---
+++ 1_datatype.ts
@@ -0,0 +1,5 @@
+let a: number = 5;
+let b: string = 'abc'
+let c: boolean = true
+console.log(a,b,c);
+export {}
